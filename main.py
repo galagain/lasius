21238c918edadfa955a651d7e881560cf3c4c695
galagain <calvin@galagain.com>
2024-02-20 15:14:15 +0100

    feat(main): add function to execute shell commands in a given directory

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+
+def execute_commands(commands: list[str], work_path: str = os.getcwd()) -> None:
+    """Executes a list of shell commands in a specified working directory.
+
+    Args:
+        commands (list[str]): A list of shell commands to be executed.
+        work_path (str): The directory in which the commands should be executed.
+                         Defaults to the current working directory of the script.
+    """
+    for command in commands:
+        # Execute each command in the specified working directory
+        subprocess.run(command, shell=True, cwd=work_path)
